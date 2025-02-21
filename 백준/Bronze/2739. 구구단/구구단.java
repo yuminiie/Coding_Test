3fc514117453c877fb83ef0f4d7394da8dcfb689
유민 <yuminiie09@naver.com>
2025-02-21 15:21:47 +0900

    [Bronze V] Title: 구구단, Time: 192 ms, Memory: 18572 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+            int N = sc.nextInt();
+            for(int A = 1; A <= 9; A++){
+                
+            System.out.println( N + " * " + A + " = " + N * A  );
+              
+            }
+        }
+}
