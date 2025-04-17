85b71ee082ef2db4ec2277ba1e76579fd71f1a05
유민 <yuminiie09@naver.com>
2025-04-17 16:55:28 +0900

    [Bronze V] Title: 시험 성적, Time: 180 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        int Score = sc.nextInt();
+        
+        if(90<=Score && Score<=100){
+            System.out.println('A');}
+        else if(80<=Score){
+            System.out.println('B');}
+        else if(70<=Score){
+            System.out.println('C');}
+        else if(60<=Score){
+            System.out.println('D');}
+        else System.out.println('F');
+       
+        
+    }
+}
