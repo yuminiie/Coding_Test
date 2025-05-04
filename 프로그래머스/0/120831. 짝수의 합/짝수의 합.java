8104da0c883a5f64526ab08af8e4ff231f235fcc
유민 <yuminiie09@naver.com>
2025-05-05 07:58:33 +0900

    [level 0] Title: 짝수의 합, Time: 0.04 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int i;
+        for( i = 1; i <= n; i++){
+            if(i%2==0){
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
