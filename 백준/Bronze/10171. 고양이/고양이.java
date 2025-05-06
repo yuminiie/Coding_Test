ab56fd55c9ac281e691cdf135ab3998e1d610d41
유민 <yuminiie09@naver.com>
2025-05-06 15:24:13 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String args[]){
+        
+        System.out.print("\\    /\\\n");
+        System.out.print(" )  ( ')\n");
+        System.out.print("(  /  )\n");
+        System.out.print(" \\(__)|");
+    }
+}
