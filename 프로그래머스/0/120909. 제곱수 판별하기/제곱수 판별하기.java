1b57ed73222127673cb9e5026b7b602213f016ed
유민 <yuminiie09@naver.com>
2025-05-22 23:27:44 +0900

    [level 0] Title: 제곱수 판별하기, Time: 3.45 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i <= n/2; i++)
+            if(i*i==n){
+                answer = 1;break;
+            }
+            else answer = 2;
+        return answer;
+    }
+}
