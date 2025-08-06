aeeb8f47892685d3d3cecffe866fcdf2532a91e1
유민 <yuminiie09@naver.com>
2025-08-07 00:46:55 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -3,16 +3,15 @@ class Solution {
         int answer = 0;
         
         if(price>=500000){
-            answer = (int)(price * 0.8);
+            return(int)(price * 0.8);
         }
         else if(price>=300000){
-            answer = (int)(price * 0.9);
+            return(int)(price * 0.9);
         }
         else if(price>=100000){
-            answer = (int)(price * 0.95);
+            return(int)(price * 0.95);
         }
-        else
-            answer = price;
-        return answer;
+        
+        return price;
     }
 }
