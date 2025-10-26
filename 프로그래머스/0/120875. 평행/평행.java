e90a92810934607aff69ef5d53c81c0b6cefc490
유민 <yuminiie09@naver.com>
2025-10-26 23:32:47 +0900

    [level 0] Title: 평행, Time: 0.04 ms, Memory: 92.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -0,0 +1,16 @@
+class Solution {
+     boolean isParallel(int[] a, int[] b, int[] c, int[] d) {
+        int dy1 = b[1] - a[1];
+        int dx1 = b[0] - a[0];
+        int dy2 = d[1] - c[1];
+        int dx2 = d[0] - c[0];
+
+        return dy1 * dx2 == dy2 * dx1;
+    }
+    public int solution(int[][] dots) {
+        if (isParallel(dots[0], dots[1], dots[2], dots[3])) return 1;
+        if (isParallel(dots[0], dots[2], dots[1], dots[3])) return 1;
+        if (isParallel(dots[0], dots[3], dots[1], dots[2])) return 1;
+        return 0;
+    }
+}
