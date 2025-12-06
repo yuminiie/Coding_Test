8bd71bf26551947eb1fdc65fc11248948219a88e
유민 <yuminiie09@naver.com>
2025-12-06 21:06:32 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 484 ms, Memory: 22048 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < i; j++){ 
+                System.out.print("*");
+            }
+            for (int j = 0; j < (N - i) * 2; j++){ 
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++){ 
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = N - 1; i >= 1; i--){
+            for (int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            for (int j = 0; j < (N - i) * 2; j++){
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++){ 
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
