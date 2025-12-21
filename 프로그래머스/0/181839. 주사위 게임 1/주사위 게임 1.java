304068bb02bdc7a03aea0c66ccb18d76f4bef539
유민 <yuminiie09@naver.com>
2025-12-22 00:40:04 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        if(a % 2 == 1 & b % 2 == 1){
+            answer = a*a + b*b;
+        } else if(a % 2 != 0 || b % 2 != 0){
+            answer = 2 * (a + b);
+        } else {
+            if((a-b) > 0){
+                answer = a - b;
+            } else {
+                answer = -1 * (a - b);
+            }
+        }
+        return answer;
+    }
+}
